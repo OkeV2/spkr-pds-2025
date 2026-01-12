2840726f40ced4e01b85e127cfbe35a3c1f0b90f
OkeV2 <ale.oquendorodriguez@gmail.com>
2026-01-12 22:15:37 +0100

    Añadir clase ErrorFrecuente
    Clase para registrar las preguntas que el usuario falla frecuentemente, permitiendo el repaso de errores.

---
+++ spkr/src/test/java/es/um/pds/spkr/modelo/ErrorFrecuente.java
@@ -0,0 +1,71 @@
+package es.um.pds.spkr.modelo;
+
+import java.util.Date;
+
+public class ErrorFrecuente {
+    
+    private Long id;
+    private Pregunta pregunta;
+    private int vecesFallada;
+    private Date fechaUltimoError;
+    
+    public ErrorFrecuente() {
+        this.vecesFallada = 0;
+        this.fechaUltimoError = new Date();
+    }
+    
+    public ErrorFrecuente(Pregunta pregunta) {
+        this.pregunta = pregunta;
+        this.vecesFallada = 1;
+        this.fechaUltimoError = new Date();
+    }
+    
+    public void incrementarErrores() {
+        this.vecesFallada++;
+        this.fechaUltimoError = new Date();
+    }
+    
+    public void reducirErrores() {
+        if (this.vecesFallada > 0) {
+            this.vecesFallada--;
+        }
+    }
+    
+    public boolean estaDominada() {
+        return this.vecesFallada == 0;
+    }
+    
+    // Getters y Setters
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public Pregunta getPregunta() {
+        return pregunta;
+    }
+    
+    public void setPregunta(Pregunta pregunta) {
+        this.pregunta = pregunta;
+    }
+    
+    public int getVecesFallada() {
+        return vecesFallada;
+    }
+    
+    public void setVecesFallada(int vecesFallada) {
+        this.vecesFallada = vecesFallada;
+    }
+    
+    public Date getFechaUltimoError() {
+        return fechaUltimoError;
+    }
+    
+    public void setFechaUltimoError(Date fechaUltimoError) {
+        this.fechaUltimoError = fechaUltimoError;
+    }
+}
