4372e8e4b8206651315f28b0298619a7ad0588a2
OkeV2 <ale.oquendorodriguez@gmail.com>
2026-01-16 06:04:56 +0100

    Corregir estrategia aleatoria para no repetir preguntas
    - Barajar índices al inicio en lugar de seleccionar aleatorio cada vez
    - Las preguntas ahora se muestran en orden aleatorio sin repeticiones

--- spkr/src/main/java/es/um/pds/spkr/estrategia/EstrategiaAleatoria.java
+++ spkr/src/main/java/es/um/pds/spkr/estrategia/EstrategiaAleatoria.java
@@ -1,23 +1,34 @@
 package es.um.pds.spkr.estrategia;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Random;
-
 import es.um.pds.spkr.modelo.Pregunta;
 
 public class EstrategiaAleatoria implements EstrategiaAprendizaje {
     
-    private Random random;
+    private List<Integer> ordenAleatorio;
+    private boolean inicializada;
     
     public EstrategiaAleatoria() {
-        this.random = new Random();
+        this.ordenAleatorio = new ArrayList<>();
+        this.inicializada = false;
     }
     
     @Override
     public Pregunta siguientePregunta(List<Pregunta> preguntas, int preguntaActual) {
+        if (!inicializada) {
+            // Crear lista de índices y barajarla
+            for (int i = 0; i < preguntas.size(); i++) {
+                ordenAleatorio.add(i);
+            }
+            Collections.shuffle(ordenAleatorio);
+            inicializada = true;
+        }
+        
         if (preguntaActual < preguntas.size()) {
-            int indiceAleatorio = random.nextInt(preguntas.size());
-            return preguntas.get(indiceAleatorio);
+            int indiceReal = ordenAleatorio.get(preguntaActual);
+            return preguntas.get(indiceReal);
         }
         return null;
     }
